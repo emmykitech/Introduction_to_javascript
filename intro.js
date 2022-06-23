dc1466984acd8027a62b59c1fff5927653ddc1f3
KiTech <emmakagbabuwe@gmail.com>
2022-06-23 20:28:56 +0100

    first commit

--- intro.js
+++ intro.js
@@ -1,6 +1,6 @@
 // Write a javascript code that prints out your name, height, and country on the screen
 var name  = "Agbabuwe Emmanuel";
 var height = "7ft";
-var country = "Nigeria"
+var country = "Nigeria";
 
 console.log("I'm " + name + ", " + height + " tall and i'm also from " + country+ ".");
